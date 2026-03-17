423fd5686dff574adab958b34fa3d66bfdac17b8
hattoku <hattoku.inamoto@gmail.com>
2026-03-18 05:49:44 +0900

    キー更新

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,4 +4,4 @@
  * このプロジェクトでは、開発の利便性のために秘密情報（APIキー等）を
  * 直接ソースコードに記述し、リポジトリに含める方針としています。
  */
-export const YOUTUBE_API_KEY = 'YOUR_YOUTUBE_API_KEY_HERE';
+export const YOUTUBE_API_KEY = 'AIzaSyAtmDhmPudGASBohQk8w-O4s6JdLGQaR5o';
