fd3b1c7dbbf4510ec499f80ee379c9bf6c6c9bd3
hattoku <hattoku.inamoto@gmail.com>
2026-03-19 05:35:47 +0900

    キー情報を変更した上でコード削除

--- lib/constants.ts
+++
@@ -1,7 +0,0 @@
-/**
- * YouTube Data API (v3) のAPIキー
- * 
- * このプロジェクトでは、開発の利便性のために秘密情報（APIキー等）を
- * 直接ソースコードに記述し、リポジトリに含める方針としています。
- */
-export const YOUTUBE_API_KEY = 'AIzaSyAtmDhmPudGASBohQk8w-O4s6JdLGQaR5o';
